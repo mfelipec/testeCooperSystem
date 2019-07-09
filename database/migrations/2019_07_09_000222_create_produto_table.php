2ee42dfb8044d465a470b92f07d18a209d4d4673
Marcus <mfelipec@gmail.com>
2019-07-08 21:10:45 -0300

    Produtos.
    - Instalando InfyOm.
    - Criando o migrate de Produtos

---
+++ database/migrations/2019_07_09_000222_create_produto_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProdutoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nome', 200)->unique();
+            $table->decimal('valor', 8, 2);
+            $table->integer('quantidade');
+            $table->boolean('situacao')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto');
+    }
+}
